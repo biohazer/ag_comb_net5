373839c4407497e4d31a970dff965bce8ed49bdf
biohazer <sfeng21@yahoo.com.au>
2021-09-16 21:02:32 +0800

    End of Section 9

--- client/src/app/_models/user.ts
+++ client/src/app/_models/user.ts
@@ -2,3 +2,36 @@ export interface User {
     username: string;
     token: string;
 }
+
+let data: number | string = "42"
+
+
+data = 42
+
+
+interface Car {
+    color: string,
+    model: string,
+    topspeed?: number
+}
+
+const car1: Car = {
+    color: "blue",
+    model: "BMW",
+}
+
+const car2: Car = {
+    color: "123",
+    model: "Mercedes"
+}
+
+
+function run(x, y):void {
+    x * y
+}
+
+const run1 = (x: number, y: number): void =>{
+    x * y
+}
+
+
